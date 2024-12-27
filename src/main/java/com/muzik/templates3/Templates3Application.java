702a4427ed3d9e52d8a0d83b722a957bd8634bab
muziktiger <jh.24workspace@gmail.com>
2024-12-27 11:52:33 +0900

    templates3 연습 완료

---
+++ src/main/java/com/muzik/templates3/Templates3Application.java
@@ -0,0 +1,13 @@
+package com.muzik.templates3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Templates3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Templates3Application.class, args);
+    }
+
+}
